76f92645eb9835993d121e65e80a2ebaa53501e2
maacaro <maacaro@gmail.com>
2018-08-06 07:34:08 -0400

    Remove unnecesary file

--- crawl.py
+++
@@ -1,9 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-
-
-def get_reviews_from():
-    process = CrawlerProcess(get_project_settings())
-
-    process.crawl('amazon-reviews-spider', product_id = 'B07DHBM5RC')
-    process.start() 
