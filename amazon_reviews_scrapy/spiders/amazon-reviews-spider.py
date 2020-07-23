002149b751c2cd964e38a837ec627488d8075c6f
Manuel Castro <maacaro@gmail.com>
2020-07-23 08:03:29 -0400

    Update selector from the spider

--- amazon_reviews_scrapy/spiders/amazon-reviews-spider.py
+++ amazon_reviews_scrapy/spiders/amazon-reviews-spider.py
@@ -3,14 +3,12 @@ import scrapy
 import urllib.parse
 import requests
 import json
-from scrapy.xlib.pydispatch import dispatcher
+
 
 class AmazonReviewsSpider(scrapy.Spider):
     name = 'amazon-reviews-spider'
 
-    def __init__(self, asin=None, *args, **kwargs):
-        super(AmazonReviewsSpider, self).__init__(*args, **kwargs)
-        
+    def __init__(self, asin=None):
         self.asin = asin
         if not asin:
             raise Exception("asin is required")
@@ -18,51 +16,56 @@ class AmazonReviewsSpider(scrapy.Spider):
         self.start_urls = ['https://www.amazon.com/product-reviews/' +
                            asin +
                            '/ref=cm_cr_arp_d_viewopt_rvwer?ie=UTF8&showViewpoints=1&pageNumber=1&reviewerType=all_reviews']
-        
-        dispatcher.connect(self._spider_opened, signals.spider_opened)
-        dispatcher.connect(self._item_passed, signals.item_passed)
-        dispatcher.connect(self._spider_closed, signals.spider_closed)
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(AmazonReviewsSpider, cls).from_crawler(
+            crawler, *args, **kwargs)
+
+        crawler.signals.connect(spider._spider_closed, signals.spider_closed)
+        crawler.signals.connect(spider._spider_opened, signals.spider_opened)
+        crawler.signals.connect(spider._item_passed, signals.item_passed)
+        return spider
 
     def _spider_opened(self):
         url = "https://maacaro-analytics-api.herokuapp.com/products"
         payload = json.dumps({
             'asin': self.asin
-            })
+        })
 
         headers = {
-             'content-type': "application/json",
-             'cache-control': "no-cache",
-         }
-        
+            'content-type': "application/json",
+            'cache-control': "no-cache",
+        }
+
         response = requests.request("POST", url, data=payload, headers=headers)
-        
+
         print("***********Product ASIN:"+self.asin)
 
-    
-    def _item_passed(self,item):
-         url = "https://maacaro-analytics-api.herokuapp.com/products/"+self.asin+"/reviews"
-         payload = json.dumps(item) 
+    def _item_passed(self, item):
+        url = "https://maacaro-analytics-api.herokuapp.com/products/"+self.asin+"/reviews"
+        payload = json.dumps(item)
 
-         headers = {
-             'content-type': "application/json",
-             'cache-control': "no-cache",
-         }
+        headers = {
+            'content-type': "application/json",
+            'cache-control': "no-cache",
+        }
 
-         response = requests.request("POST", url, data=payload, headers=headers)
+        response = requests.request("POST", url, data=payload, headers=headers)
 
-         print(response.text)
+        print(response.text)
 
     def _spider_closed(self):
         url = "https://maacaro-analytics-api.herokuapp.com/products/"+self.asin
         payload = json.dumps({'name': self.productName})
 
         headers = {
-             'content-type': "application/json",
-             'cache-control': "no-cache",
-         }
-        
+            'content-type': "application/json",
+            'cache-control': "no-cache",
+        }
+
         response = requests.request("PUT", url, data=payload, headers=headers)
-        
+
         print("***********Product Name:"+self.productName)
 
     def parse(self, response):
@@ -72,9 +75,10 @@ class AmazonReviewsSpider(scrapy.Spider):
 
     def parse_reviews(self, response):
         yield from self.extract_reviews(response)
-    
-    def extract_product_name(self,response):
-        self.productName = response.css('div[class = "a-row product-title"] h1 a::text').extract_first()
+
+    def extract_product_name(self, response):
+        self.productName = response.css(
+            'div[class = "a-row product-title"] h1 a::text').extract_first()
 
     def extract_reviews(self, response):
         for review in response.css('div[data-hook=review]'):
@@ -82,12 +86,12 @@ class AmazonReviewsSpider(scrapy.Spider):
                 'asin': self.asin,
                 'id': review.xpath('@id').extract_first(),
                 'stars': self.extract_stars(review),
-                'title': review.css('a.review-title::text').extract_first(),
-                'author_profile_url': review.css('a[data-hook="review-author"]::attr(href)').extract_first(),
-                'author_name': review.css('a[data-hook="review-author"]::text').extract_first(),
+                'title': review.css('a.review-title span::text').extract_first(),
+                'author_profile_url': review.css('a.a-profile::attr(href)').extract_first(),
+                'author_name': review.css('a.a-profile::text').extract_first(),
                 'badges': review.css('span.c7y-badge-text::text').extract(),
                 'review_date': review.css('span.review-date::text').extract_first(),
-                'review_text': '\n'.join(review.css('span.review-text::text').extract()),
+                'review_text': '\n'.join(review.css('span.review-text span::text').extract()),
                 'comments_count': review.css('span.review-comment-total::text').extract_first(),
                 'review_helpful_votes': self.extract_review_votes(review)
             }
@@ -103,7 +107,8 @@ class AmazonReviewsSpider(scrapy.Spider):
 
     def extract_stars(self, review):
         stars = None
-        star_classes = review.css('i.a-icon-star::attr(class)').extract_first().split(' ')
+        star_classes = review.css(
+            'i.a-icon-star::attr(class)').extract_first().split(' ')
         for i in star_classes:
             if i.startswith('a-star-'):
                 stars = int(i[7:])
@@ -113,7 +118,7 @@ class AmazonReviewsSpider(scrapy.Spider):
     def extract_pages(self, response):
         page_links = response.css('span[data-action="reviews:page-action"] li')
         base_parts = urllib.parse.urlsplit(self.start_urls[0])
-        
+
         if len(page_links) > 2:
             last_page_url = page_links[-2].css('a::attr(href)').extract_first()
             url_parts = urllib.parse.urlsplit(last_page_url)
@@ -131,6 +136,3 @@ class AmazonReviewsSpider(scrapy.Spider):
                     url_parts[3] = urllib.parse.urlencode(qs, doseq=True)
                     self.logger.info('url ' + repr(url_parts))
                     yield scrapy.Request(urllib.parse.urlunsplit(url_parts), self.parse_reviews)
-
-
-
